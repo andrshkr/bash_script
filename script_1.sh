d49299f08b7387f7ec57a940a4585bd05a96f344
Andrei Shakuro <andrei.shakuro@emlid.com>
2020-09-15 19:25:37 +0300

    Fix troble wits names with spaces

--- script_1.sh
+++ script_1.sh
@@ -8,13 +8,35 @@ PID_MY_SHELL="$(echo "$$")"
 ps "${PID_MY_SHELL}" | tail -1 | cut -b 29-38
 
 
-cd ${DIR_} 
+cd ${DIR_}
+
+
+N="$(find . -maxdepth 1 -name "* *" | wc -l)"
+echo "${N}"
+if [ ${N} -gt 0 ]
+then
+	STR="$(find . -name "* *" | sed 's!.*/!!')"
+
+	COUNTER=0
+    while [  $COUNTER -lt ${N} ]; do
+        #echo The counter is $COUNTER
+        let COUNTER=COUNTER+1
+        file "$(echo "${STR}" | sed -n -e ${COUNTER}p)"
+    done
+
+	
+else
+	echo "No files with spaces! "
+fi
+
+
+
 
 if [ -z $2 ]
 	then
-	file $( ls -LA -1 ${DIR_}) 
+	file $( ls -LA -1 -I "* *" ${DIR_}) 
 else
-	file $( ls -LA -1 ${DIR_})  | grep $2: 
+	file $( ls -LA -1 -I "* *" ${DIR_})  | grep $2: 
 fi
 
 
